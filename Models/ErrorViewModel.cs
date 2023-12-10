a3ccddaf6de89a9a7d70f7d7dce9ced70324d6d2
yannick.vetter <yannick.vetter@SCL-QUBB0150-02>
2023-12-10 16:10:43 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YaustinMusicShopOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
