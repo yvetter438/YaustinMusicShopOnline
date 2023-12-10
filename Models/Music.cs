f67f4ad70cdc71288aa7c55a0a22ab80b3acf576
yvetter438 <yannick.vetter@outlook.com>
2023-12-10 16:40:01 -0600

    Added Models

---
+++ Models/Music.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace YaustinMusicShopOnline.Models
+{
+    public class Music
+    {
+        public required int MusicId { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public required string Title { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public required string Artist { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public required string Genre { get; set; }
+
+      //  [StringLength(60, MinimumLength = 3)]
+      //  [Required]
+      //  public required string Type { get; set; }
+
+        [Range(0, 100)]
+        [DataType(DataType.Currency)]
+        public required decimal Price { get; set; }
+
+     
+
+    }
+}
---
+++ Models/MusicGenreModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+
+namespace YaustinMusicShopOnline.Models
+{
+    public class MusicGenreModel
+    {
+        public List<Music>? Musics { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? MusicGenre { get; set; }
+
+        public SelectList? Artists { get; set; }
+        public string? MusicArtist { get; set; }
+
+
+    }
+}
